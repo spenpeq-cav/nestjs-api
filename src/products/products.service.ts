44e073c0f5d2140fd1ffea75a19d0724806f6a4d
Spencer Pequegnat <spencer.peq@gmail.com>
2022-06-08 19:53:10 -0700

    update and delete products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -18,10 +18,36 @@ export class ProductsService {
   }
 
   getSingleProduct(productId: string) {
-    const product = this.products.find((prod) => prod.id === productId);
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.description = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(productId: string) {
+    const index = this.findProduct(productId)[1];
+    this.products.splice(index, 1);
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
     if (!product) {
       throw new NotFoundException('Could not find product');
     }
-    return { ...product };
+    return [product, productIndex];
   }
 }
